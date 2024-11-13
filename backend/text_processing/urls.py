15a6542e230982ed39b91b8734def3e842932489
FlashLLan <lasnkyalex888776@gmail.com>
2024-11-13 08:51:54 +0200

    starting message_storege process

--- backend/text_processing/urls.py
+++ backend/text_processing/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('api/process-message/', views.process_message, name='process_message'),
+    path('api/message_store/', views.get_message_storage, name='message_store'),
 ]
