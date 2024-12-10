acb7d2f431a18bc55f8df967f6fe35a1d5a2b734
Kyrylo Kolesnichenko <kykocq@gmail.com>
2024-12-10 03:25:56 +0200

    message edit back

--- backend/style_personalization/urls.py
+++ backend/style_personalization/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from style_personalization.views import QuestionView, SaveStyleView, DeleteStyleView
+from style_personalization.views import QuestionView, SaveStyleView, DeleteStyleView, ListStylesView
 
 urlpatterns = [
     path('questions/', QuestionView.as_view(), name='questions'),
     path('styles/save/', SaveStyleView.as_view(), name='styles_save' ),
     path('styles/delete/', DeleteStyleView.as_view(), name='delete_style'),
+    path('styles/retrieve/', ListStylesView.as_view(), name='user-styles-list'),
 ]
