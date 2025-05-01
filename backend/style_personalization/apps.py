244f640e4740294b005bdbe249c14978560bb959
FlashLLan <lasnkyalex888776@gmail.com>
2025-05-01 18:30:10 +0100

    frontend draft

--- backend/style_personalization/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class StylePersonalizationConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'style_personalization'
--- backend/text_processing/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-
-from text_processing.views import GenerateMessageView
-
-urlpatterns = [
-    path('chat-gpt/call/', GenerateMessageView.as_view() , name='chat-gpt-call'),
-]
